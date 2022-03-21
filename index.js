60b909950029c32535443caf40cafe7c447ae3fc
Seunghwan <ysp1307@naver.com>
2022-03-22 00:11:56 +0900

    feat: 요구사항분석

---
+++ index.js
@@ -0,0 +1,33 @@
+// // step1 요구사항 정리
+// To-do 메뉴 추가
+// - [] 메뉴의 이름을 입력받고 확인 버튼 누르면 메뉴 추가
+// - [] 메뉴의 이름을 입력받고 엔터키로 메뉴 추가
+//- [] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
+//- [] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+// To-do 수정 추가
+// - [] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴 수정 모달창이 뜬다.
+// - [] 모달창에서 신규메뉴명을 입력 받고, 확인 버튼으로 메뉴가 수정된다.
+
+// To-do 삭제 추가
+// - [] 메뉴 삭제 버튼클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [] 확인 버튼 클릭시 메뉴가 삭제된다.
+// - [] 총 메뉴 갯수를 count하여 상단에 보여준다.
+
+// <li class="menu-list-item d-flex items-center py-2">
+//   <span class="w-100 pl-2 menu-name">${name}</span>
+//   <button
+//     type="button"
+//     class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+//   >
+//     수정
+//   </button>
+//   <button
+//     type="button"
+//     class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+//   >
+//     삭제
+//   </button>
+// </li>
